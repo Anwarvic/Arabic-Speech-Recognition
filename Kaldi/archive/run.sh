37927f42f28f5337b1dbea6ab5691e1ffc10d3f3
anwarvic <mohamedanwarvic@gmail.com>
2019-03-24 14:10:44 +0200

    edited archive/run.sh to enable validation script

--- Kaldi/archive/run.sh
+++ Kaldi/archive/run.sh
@@ -29,10 +29,17 @@ echo
 # Making feats.scp files
 mfccdir=mfcc
 # Uncomment and modify arguments in scripts below if you have any problems with data sorting
-# utils/validate_data_dir.sh data/train     # script for checking prepared data - here: for data/train directory
-# utils/fix_data_dir.sh data/train          # tool for data proper sorting if needed - here: for data/train directory
+utils/validate_data_dir.sh data/train     # script for checking prepared data - here: for data/train directory
+utils/fix_data_dir.sh data/train          # tool for data proper sorting if needed - here: for data/train directory
+#OLD
 steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" data/train exp/make_mfcc/train $mfccdir
 steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" data/test exp/make_mfcc/test $mfccdir
+#ANWAR
+# for x in train test; do 
+#  steps/make_mfcc.sh --nj 1 data/$x exp/make_mfcc/$x mfcc
+#  steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x mfcc
+#  utils/fix_data_dir.sh data/$x
+# done
 # Making cmvn.scp files
 steps/compute_cmvn_stats.sh data/train exp/make_mfcc/train $mfccdir
 steps/compute_cmvn_stats.sh data/test exp/make_mfcc/test $mfccdir
