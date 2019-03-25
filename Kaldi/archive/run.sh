801fdf8ed5558837846381c48331b0b5ad298f8e
anwarvic <mohamedanwarvic@gmail.com>
2019-03-25 10:48:24 +0200

    minor changes in the comments

--- Kaldi/archive/run.sh
+++ Kaldi/archive/run.sh
@@ -77,24 +77,24 @@ echo
 lang=data/lang
 arpa2fst --disambig-symbol=#0 --read-symbol-table=$lang/words.txt $local/tmp/lm.arpa $lang/G.fst
 echo
-echo "===== MONO TRAINING ====="
+echo "===== HMM MONO TRAINING ====="
 echo
 steps/train_mono.sh --nj $nj --cmd "$train_cmd" data/train data/lang exp/mono  || exit 1
 echo
-echo "===== MONO DECODING ====="
+echo "===== HMM MONO DECODING ====="
 echo
 utils/mkgraph.sh --mono data/lang exp/mono exp/mono/graph || exit 1
 steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/mono/graph data/test exp/mono/decode
 echo
-echo "===== MONO ALIGNMENT ====="
+echo "===== HMM MONO ALIGNMENT ====="
 echo
 steps/align_si.sh --nj $nj --cmd "$train_cmd" data/train data/lang exp/mono exp/mono_ali || exit 1
 echo
-echo "===== TRI1 (first triphone pass) TRAINING ====="
+echo "===== HMM TRI1 (first triphone pass) TRAINING ====="
 echo
 steps/train_deltas.sh --cmd "$train_cmd" 2000 11000 data/train data/lang exp/mono_ali exp/tri1 || exit 1
 echo
-echo "===== TRI1 (first triphone pass) DECODING ====="
+echo "===== HMM TRI1 (first triphone pass) DECODING ====="
 echo
 utils/mkgraph.sh data/lang exp/tri1 exp/tri1/graph || exit 1
 steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/tri1/graph data/test exp/tri1/decode
